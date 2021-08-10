7c43562b86387e5b3ae6073824462c815bb5bddd
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-10 16:41:30 -0300

    adiciona script de introducao ao typescript

---
+++ Modulo-6-TypeScript/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+const myNum1 = 10;
+const myNum2 = 5;
+
+console.log(sum(myNum1, myNum2));
---
+++ Modulo-6-TypeScript/index.ts
@@ -0,0 +1,8 @@
+function sum(num1: number, num2: number) {
+  return num1 + num2
+}
+
+const myNum1 = 10;
+const myNum2 = 5;
+
+console.log(sum(myNum1, myNum2));
