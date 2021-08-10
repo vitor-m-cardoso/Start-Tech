de6f5db0330d17dbca25789a21acfb07c54f8932
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-10 15:11:00 -0300

    adiciona script de aula sobre template string

---
+++ Modulo-5-JS-Intermediario/template-string.js
@@ -0,0 +1,5 @@
+let meuNome = 'Vitor';
+let meuSobrenome = 'Cardoso';
+let minhaProfissao = 'Full-Stack Web Developer';
+
+console.log(`Bom dia! Eu sou o ${meuNome} ${meuSobrenome}. Minha profissao é: ${minhaProfissao}.`);
