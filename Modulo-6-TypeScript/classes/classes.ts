e32b070c566e5c7e49e31aa592a13bbc327b520e
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-12 13:27:54 -0300

    adiciona aula de modificadores de acesso ao script classes

--- Modulo-6-TypeScript/classes/classes.ts
+++ Modulo-6-TypeScript/classes/classes.ts
@@ -15,3 +15,31 @@ const data = new Data(1, 1, 2020);
 console.log(data.dia);
 
 const data2 = new Data(1, 1);
+
+// Exemplo de Modificadores de acesso
+
+class Carro {
+  private velocidadeAtual: number = 0;
+
+  constructor (
+    public marca: string,
+    public modelo: string,
+    private velocidadeMaxima: number = 220,
+  ) {}
+
+  private alterarVelocidade(delta: number) {
+    // VALIDAR ACELERACAO E RENAGEM
+    // this.velocidadeAtual = xxx
+  }
+
+  acelerar() {
+    this.alterarVelocidade(5);
+  }
+
+  frear() {
+    this.alterarVelocidade(-5);
+  }
+}
+
+const carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelerar();
