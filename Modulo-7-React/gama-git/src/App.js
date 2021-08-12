679d44840a6f8922ffb1af9a1f762e441911f69f
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-12 15:19:19 -0300

    inicia projeto gama-git do modulo de React

---
+++ Modulo-7-React/gama-git/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <input name="user" className="inputUser" placeholder="Usuário" />
+      <button type="button">Pesquisar</button>
+    </>
+  );
+}
+
+export default App;
