9e94ee2a9424143ba7277731388ecba5566b7519
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-12 13:39:23 -0300

    adiciona aula de heranca a script de classes

--- Modulo-6-TypeScript/classes/classes.ts
+++ Modulo-6-TypeScript/classes/classes.ts
@@ -17,7 +17,6 @@ console.log(data.dia);
 const data2 = new Data(1, 1);
 
 // Exemplo de Modificadores de acesso
-
 class Carro {
   private velocidadeAtual: number = 0;
 
@@ -28,8 +27,13 @@ class Carro {
   ) {}
 
   private alterarVelocidade(delta: number) {
-    // VALIDAR ACELERACAO E RENAGEM
-    // this.velocidadeAtual = xxx
+    const novaVelodidade = this.velocidadeAtual + delta;
+
+    if (novaVelodidade >= 0 && novaVelodidade <= this.velocidadeMaxima) {
+      this.velocidadeAtual = novaVelodidade;
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+    }
   }
 
   acelerar() {
@@ -43,3 +47,21 @@ class Carro {
 
 const carro = new Carro('Chevrolet', 'Prisma', 250);
 carro.acelerar();
+
+// Exemplo do conceito de Heranca
+class Camaro extends Carro { // herda tudo da classe Carro
+  private turbo = false;
+
+  constructor() {
+    super('Chevrolet', 'Camaro', 280); // utilizado para chamar a classe pai para utilizar suas propriedades
+  }
+
+  ligarTurbo() {
+    this.turbo = true;
+  }
+}
+
+const camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
