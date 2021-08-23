6d2781a329d4b7c4db966bb6c330b36bf949c20d
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-23 12:25:45 -0300

    transformando projeto em react

--- Modulo-8-GraphQL/dev-demands/packages/server/src/main.js
+++ Modulo-8-GraphQL/dev-demands/packages/server/src/main.js
@@ -1,6 +1,4 @@
 import { createServer } from 'http';
-import { readFile } from 'fs'; // file sistem
-import { resolve } from 'path' // modulo nativo do node
 import { parse } from 'querystring'; // modulo nativo do node
 
 // configuracoes do servidor e respostas
@@ -16,35 +14,6 @@ const server = createServer((request, response) => {
       response.end();
       break;
     }
-    case '/sign-in': {
-      const path = resolve(__dirname, './pages/sign-in.html');
-      readFile(path, (error, file) => {
-        if (error) {
-          response.write(500, 'Can\'t process HTML file.')
-          response.end();
-          return;
-        }
-
-        response.writeHead(200);
-        response.write(file);
-        response.end();
-      });
-      break;
-    }
-    case '/home': {
-      const path = resolve(__dirname, './pages/home.html');
-      readFile(path, (error, file) => {
-        if (error) {
-          response.write(500, 'Can\'t process HTML file.')
-          response.end();
-          return;
-        }
-        response.writeHead(200);
-        response.write(file);
-        response.end();
-      });
-      break;
-    }
     case '/authenticate': {
       let data = '';
       request.on('data', (chunk) => {
@@ -52,9 +21,7 @@ const server = createServer((request, response) => {
       });
       request.on('end', () => {
         const params = parse(data);
-        response.writeHead(301, {
-          Location: '/home',
-        });
+        
         response.end();
       });
       break;
